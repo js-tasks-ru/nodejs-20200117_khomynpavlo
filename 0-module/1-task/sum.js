f95a491e77358c9dab1352ee7a99e510e5e74ca5
User1 <h@kkk.com>
2020-01-12 22:32:47 +0200

    add solution for 1-module 1-task

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,5 +1,10 @@
 function sum(a, b) {
-  /* ваш код */
+  if (typeof a == 'number' && typeof b == 'number') {
+    return a + b;
+  } else {
+    throw new TypeError('Arguments are not numbers');
+  }
 }
+sum(2, 3);
 
 module.exports = sum;
