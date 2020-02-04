fbb3d0c94eef1acc9c0407dfe27c78d9fa9714d1
User1 <h@kkk.com>
2020-02-04 20:25:45 +0200

    Split stream

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -7,10 +7,28 @@ class LineSplitStream extends stream.Transform {
   }
 
   _transform(chunk, encoding, callback) {
+    const lines = ((this.last != null ? this.last : '') + chunk.toString()).split(`${os.EOL}`);
+    this.last = lines.pop();
+    lines.forEach(line => this.push(line));
+    callback();
   }
 
-  _flush(callback) {
+ _flush(callback) {
+   this.push(this.last != null ? this.last: '')
+    callback();
   }
 }
+
+const lines = new LineSplitStream({ encoding: 'utf-8',});
+
+function onData(line) {
+  console.log(line);
+}
+
+lines.on('data', onData);
+
+lines.write(`first line${os.EOL}second line${os.EOL}third line`);
+
+lines.end();
 
 module.exports = LineSplitStream;
